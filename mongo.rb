6f40603b60923ae3ca1be2d2e359d3310832bd7b
Dan Ellis <Dan@Dans-MacBook-Pro.local>
2014-08-19 19:14:01 +0100

    Adding user document for monogo. changing date of the weight to the DateTime field.

--- mongo.rb
+++ mongo.rb
@@ -4,6 +4,14 @@ include Mongo
 class Weight
   include Mongoid::Document
   field :user, type: String
-  field :date, type: Date
+  field :date, type: DateTime
   field :weight, type: Float
+end
+
+class User
+  include Mongoid::Document
+  field :user, type: String
+  field :full_name, type: String
+  field :height, type: Float
+  field :color, type: String
 end
